97220d1e0d36648ca20716f770b301742831630b
Robert LUTT <ausdauerr@gmail.com>
2017-04-21 12:42:06 -0700

    update to ft_db.c. Going to get a drink! brb

--- ft_db.c
+++ ft_db.c
@@ -6,22 +6,23 @@
 /*   By: rlutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 12:00:02 by rlutt             #+#    #+#             */
-/*   Updated: 2017/04/21 12:20:12 by rlutt            ###   ########.fr       */
+/*   Updated: 2017/04/21 12:41:17 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_db.h"
 
-int			ft_db(int ac, char **args)
-{
-	
-}
-
 int			main(int ac, char **av)
 {
+	t_dbnfo		db;
+
 	if (ac > 1)
 	{
-		ft_db(ac - 1, &av[1]);
+		db_inidbnfo(&db);
+		/*1.Get arguments from user.
+		*  2.If there is DB already. Load it.
+		*   3. Carry out operation given by user.
+		*/
 		return (1);
 	}
 	return (0);
