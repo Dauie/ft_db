e52c00f1e74a7df84690790a5477ed419923dc39
Keenan ROMAIN <kromain@e1z2r4p12.42.us.org>
2017-04-20 18:45:41 -0700

    coolio

--- db_inistructs.c
+++ db_inistructs.c
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_db.h"
 
+static t_dbnode *do_initnode(t_dbnode *elem)
+{
+	elem->wght = 0;
+	elem->data.tbl_ctime = 0;
+	elem->data.tbl_mtime = NULL;
+	elem->data.bse_tbl = NULL;
+	elem->data.num_tbl = NULL;
+	elem->left = NULL;
+	elem->right = NULL;
+	elem->tbl_name = NULL;
+	return (elem);
+}
