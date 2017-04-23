096d5eb42733ebeb9cf439f80ee3fc238e666e84
Robert LUTT <ausdauerr@gmail.com>
2017-04-22 19:51:08 -0700

    update for db_print

---
+++ db_printdb.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   db_printdb.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlutt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/22 19:40:14 by rlutt             #+#    #+#             */
+/*   Updated: 2017/04/22 19:50:19 by rlutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_db.h"
+
+/*Prints the meta data for the database*/
+void	db_printdbmeta(t_dbnode **tree)
+{
+
+}
+
+/*Prints each table name*/
+void	db_printdb(t_dbnode **t_tree)
+{
+
+}
+
+/*prints a tables metadata*/
+void	db_printtblmeta(t_dbnode **t_tree, char *tbl_name)
+{
+}
+/*Prints the table's entry names*/
+void	db_printtbl(t_dbnode **t_tree, char *tbl_name)
+{
+
+}
+/*prints a entry's metadata*/
+void	db_printentrymeta(t_dbnode **t_tree, char *ename)
+{
+
+}
+/*Prints the entry's values*/
+void	db_printentry(t_dbnode *t_tree, char *ename)
+{
+
+}
+
