1b12b7081e7276db83f6006b65dc7f99b131aea1
Keenan ROMAIN <kromain@e1z2r5p29.42.us.org>
2017-04-22 17:59:12 -0700

    small changes

--- db_loaddatabase.c
+++ db_loaddatabase.c
@@ -5,6 +5,17 @@ t_dbnode	*db_loaddatabase(t_dbnfo db)
 	t_dbnode *tree;
 	FILE *p_file;
 
-	p_file = fopen( "rickytickytappy.db", );
+	if (!(tree = (t_dbnode *)malloc(sizeof(t_dbnode))))
+		return (NULL);
+	if (!(p_file = fopen( "rickytickytappy.db", "r")))
+	{
+		tree.tblnam = NULL;
+		tree.tbl_ctime = 0;
+		return (tree);
+	}
+	else
+	{
+		
+	}
 	return(tree);
 }
