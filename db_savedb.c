e89c470d58bcf233b0294720da1659ed82c66355
Keenan ROMAIN <kromain@e1z2r5p29.42.us.org>
2017-04-22 19:47:36 -0700

    formatting for saving functions

---
+++ db_savedb.c
@@ -0,0 +1,16 @@
+#include "ft_db.h"
+
+void	db_savedb(t_dbnode *tree)
+{
+
+}
+
+void	db_savetbl(t_dbnode *t_tree, char *tbl_name)
+{
+
+}
+
+void	db_saveentry(t_dbnode *t_tree, char *ename)
+{
+
+}
