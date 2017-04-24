817d426e1e2ff5eeea17f8039ea4b150a6b9b0a1
Robert LUTT <ausdauerr@gmail.com>
2017-04-24 12:47:23 -0700

    update to print

--- db_printdb.c
+++ db_printdb.c
@@ -6,7 +6,7 @@
 /*   By: rlutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/22 19:40:14 by rlutt             #+#    #+#             */
-/*   Updated: 2017/04/22 19:50:19 by rlutt            ###   ########.fr       */
+/*   Updated: 2017/04/24 12:47:11 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,27 @@ void	db_printdb(t_dbnode **t_tree)
 /*prints a tables metadata*/
 void	db_printtblmeta(t_dbnode **t_tree, char *tbl_name)
 {
+	
 }
 /*Prints the table's entry names*/
 void	db_printtbl(t_dbnode **t_tree, char *tbl_name)
 {
 	printf("%s\n", tbl_name);
+	db_printttree(*t_tree);
 }
 /*prints a entry's metadata*/
-void	db_printentrymeta(t_dbnode **t_tree, char *ename)
+void	db_printentrymeta(t_enode *entry)
 {
-
+	printf("Entry Name: %s", entry->ename);
+	printf("Last Modified: %s\n", ctime(&entry->emodtime));
+	printf("Created: %s\n", ctime(&entry->ecretime));
 }
 /*Prints the entry's values*/
-void	db_printentry(t_dbnode *t_tree, char *ename)
+void	db_printentry(t_enode *entry)
 {
-	printf("%s\n", ename);
-	printf("%lld\n", (long long)t_tree->entries->emodtime);
-	printf("%lld\n", (long long)t_tree->entries->ecretime);
-	printf("%lld\n", (long long)t_tree->entries->nmembr);
-	printf("%s\n", (char *)t_tree->entries->cmembr);
+	printf("%s", entry->ename);
+	int i = -1;
+	while (entry->cmembr[++i])
+		printf("%s", entry->cmembr[i]);
 }
 
--- db_printtree.c
+++ db_printtree.c
@@ -6,30 +6,52 @@
 /*   By: rlutt <rlutt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 19:08:20 by rlutt             #+#    #+#             */
-/*   Updated: 2017/04/20 18:46:53 by rlutt            ###   ########.fr       */
+/*   Updated: 2017/04/24 12:35:42 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_db.h"
 
-void 	ls_printtree(t_dbnode *tree)
+void 	db_printttree(t_dbnode *t_tree)
 {
-	if (!tree)
+	if (!t_tree)
 		return ;
-	if (tree->left)
-		ls_printtree(tree->left);
-	printf("%s\t", tree->tbl_name);
-	if (tree->right)
-		ls_printtree(tree->right);
+	if (t_tree->left)
+		ls_printtree(t_tree->left);
+	printf("%s\n", t_tree->tbl_name);
+	if (t_tree->right)
+		ls_printtree(t_tree->right);
 }
 
-void 	ls_revprinttree(t_dbnode *tree)
+void 	db_revprintttree(t_dbnode *t_tree)
 {
-	if (!tree)
+	if (!t_tree)
 		return ;
-	if (tree->right)
-		ls_revprinttree(tree->right);
-	printf("%s  ", tree->tbl_name);
-	if (tree->left)
-		ls_revprinttree(tree->left);
+	if (t_tree->right)
+		ls_revprinttree(t_tree->right);
+	printf("%s\n", t_tree->tbl_name);
+	if (t_tree->left)
+		ls_revprinttree(t_tree->left);
+}
+
+void 	db_printetree(t_enode *e_tree)
+{
+	if (!e_tree)
+		return ;
+	if (e_tree->left)
+		db_printetree(e_tree->left);
+	printf("%s\n", e_tree->ename);
+	if (e_tree->right)
+		db_printetree(e_tree->right);
+}
+
+void 	db_revprintetree(t_enode *e_tree)
+{
+	if (!e_tree)
+		return ;
+	if (e_tree->right)
+		db_revprintetree(e_tree->right);
+	printf("%s\n", e_tree->ename);
+	if (e_tree->left)
+		db_revprintetree(e_tree->left);
 }
