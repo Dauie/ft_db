4b6e539110f5e65725873935c9d4a03dba6661bc
Robert LUTT <ausdauerr@gmail.com>
2017-05-04 12:29:25 -0700

    issues with printing fixed

--- db_printdb.c
+++ db_printdb.c
@@ -6,7 +6,7 @@
 /*   By: rlutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/22 19:40:14 by rlutt             #+#    #+#             */
-/*   Updated: 2017/05/04 12:21:48 by rlutt            ###   ########.fr       */
+/*   Updated: 2017/05/04 12:27:12 by rlutt            ###   ########.fr       */
 /*   Updated: 2017/05/01 14:37:33 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -32,27 +32,6 @@ void	db_printhelp(void)
 	printf("	|  \033[01;31m-pe\033[0m :   print entry          |\n");
 	printf("	|  \033[01;31m-xe\033[0m :   export db            |\n");
 	printf("	_________________________________\n");
-	printf("./ft_db -mode [table] [key] [value / new value ...]\n");
-	printf("	________________________________\n");
-	printf("	%s                               %s\n", G_DLIN, G_DLIN);
-	printf("	%s   \033[22;31m%s\033[0m      \033[01;31mZEN\033[0m \033[22;31m%s\033[0m  \033[01;31mDATA\033[0m      \033[22;31m%s\033[0m   %s\n", G_DLIN,  G_EDIV, G_TSYM, G_EDIV, G_DLIN);
-	printf("	%s                               %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[01;31m%s\033[0m :	table symbol            %s\n", G_DLIN, G_TSYM, G_DLIN);
-	printf("	%s  \033[01;31m%s\033[0m :	entry symbol            %s\n", G_DLIN, G_ESYM, G_DLIN);
-	printf("	%s  \033[22;31m-_-_-_-_\033[0m \033[01;31mLegend\033[0m \033[22;31m_-_-_-_-\033[0m     %s\n", G_DLIN, G_DLIN);
-	printf("	%s                               %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-ae\033[0m :   add entry            %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-dt\033[0m :   delete table         %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-de\033[0m :   delete entry         %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-dv\033[0m :   delete entry's value %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-pd\033[0m :   print entire DB      %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-pt\033[0m :   print table          %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-ptm\033[0m:   print table info     %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-pe\033[0m :   print entry          %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-xt\033[0m :   export table         %s\n", G_DLIN, G_DLIN);
-	printf("	%s  \033[22;31m-xe\033[0m :   export entry         %s\n", G_DLIN, G_DLIN);
-	printf("	%s                               %s\n", G_DLIN, G_DLIN);
-	printf("	%s_______________________________%s\n", G_DLIN, G_DLIN);
 }
 
 void	db_printdb(t_tnode *t_tree)
